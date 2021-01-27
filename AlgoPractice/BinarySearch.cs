e2d60b60958f8214a88eaea5b594aefe6ee90c4a
Jason-Wynkoop <wynkjm01@pfw.edu>
2021-01-27 12:55:26 -0500

    Initial Commit

---
+++ AlgoPractice/BinarySearch.cs
@@ -0,0 +1,37 @@
+﻿
+namespace AlgoPractice
+{
+    public class BinarySearch
+    {
+        public BinarySearch()
+        {
+            
+        }
+
+        public int binarySearch(int[] arr, int min, int max, int x)
+        {
+            
+            //if this is false, target does not exist. 
+            if (max >= min)
+            {
+                
+                //Setting up mid
+                int mid = min + (max - min) / 2;
+
+                //if mid == max then target found at mid
+                if (mid == x) return mid;
+                
+                //if number at mid is greater than the target, then return left side of array
+                if (arr[mid] > x)
+                {
+                    return binarySearch(arr, min, mid - 1, x);
+                }
+
+                //if number at mid is less than the target, then return right side of array
+                return binarySearch(arr, mid + 1, max, x);
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ AlgoPractice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AlgoPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {1, 2, 3, 4, 4, 5, 6, 8};
+            int l = 0;
+            int r = array.Length - 1;
+            int x = 6;
+
+            Console.WriteLine(new BinarySearch().binarySearch(array, l, r, x));
+        }
+    }
+}
