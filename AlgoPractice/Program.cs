ad69e5f008cddbaad90080394124dcbda2da8b76
Jason-Wynkoop <wynkjm01@pfw.edu>
2021-01-27 15:16:39 -0500

    Added QuickSort.cs

--- AlgoPractice/Program.cs
+++ AlgoPractice/Program.cs
@@ -1,16 +1,42 @@
 ﻿using System;
+using System.Collections.Concurrent;
 
 namespace AlgoPractice
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            int[] array = {1, 2, 3, 4, 4, 5, 6, 9, 10, 14, 67};
+            int[] array = GenArray();
+            PrintArray(array);
+            array = new QuickSort().quickSort(array, 0, array.Length - 1);
+            PrintArray(array);
             int l = 0;
             int r = array.Length - 1;
-            int x = 10;
-            Console.WriteLine(new BinarySearch().binarySearch(array, l, r, x));
+            int x = 0;
+            Console.WriteLine("\n" + new BinarySearch().binarySearch(array, l, r, x));
+        }
+
+        private static int[] GenArray()
+        {
+            Random random = new Random();
+            int[] arr = new int[50];
+            for (int i = 0; i < 50; i++)
+            {
+                arr[i] = random.Next(50);
+            }
+
+            return arr;
+        }
+
+        private static void PrintArray(int[] arr)
+        {
+            
+            Console.WriteLine();
+            foreach (var t in arr)
+            {
+                Console.Write(t + ", ");
+            }
         }
     }
 }
---
+++ AlgoPractice/QuickSort.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Concurrent;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks.Dataflow;
+
+namespace AlgoPractice
+{
+    public class QuickSort
+    {
+
+        /// <summary>
+        ///The main function that implements quick sort.
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="low"></param>
+        /// <param name="high"></param>
+        /// <returns></returns>
+        public int[] quickSort(int[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                //pivot_index is partitioning index, arr[pivot_index] is now at correct place in sorted array
+                int pivotIndex = Partition(arr, low, high);
+
+                quickSort(arr, low, pivotIndex - 1); //before pivot index
+                quickSort(arr, pivotIndex + 1, high); //After pivot index
+            }
+
+            return arr;
+        }
+
+        /// <summary>
+        ///The function selects the last element as pivot element, places that pivot element correctly in the array
+        /// in such a way that all the elements to the left of the pivot are lesser than the pivot and all
+        /// the elements to the tight of pivot are greater than it.
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="low"></param>
+        /// <param name="high"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        private int Partition(int[] arr, int low, int high)
+        {
+            //pivot - Element at right most position
+            int pivot = arr[high];
+            
+            int i = (low - 1); //Index of smaller element
+            for (int j = low; j < high; j++)
+            {
+                //If the current element is smaller than the pivot, swap the element with pivot
+                if (arr[j] < pivot)
+                {
+                    i++;
+                    
+                    //swap arr[i] with arr[j]
+                    int temp1 = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp1;
+                }
+            }
+            
+            //swap arr[i+1] and arr[high] (or pivot)
+            int temp = arr[i + 1];
+            arr[i + 1] = arr[high];
+            arr[high] = temp;
+
+
+            return i + 1;
+        }
+    }
+}
