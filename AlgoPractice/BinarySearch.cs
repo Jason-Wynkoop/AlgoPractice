9824195cb30d274f1c90a1e0fd57c65fd5d9bb13
Jason-Wynkoop <wynkjm01@pfw.edu>
2021-01-27 14:26:10 -0500

    Small change

--- AlgoPractice/BinarySearch.cs
+++ AlgoPractice/BinarySearch.cs
@@ -12,23 +12,20 @@ namespace AlgoPractice
         {
             
             //if this is false, target does not exist. 
-            if (max >= min)
+            if (max > min)
             {
                 
                 //Setting up mid
                 int mid = min + (max - min) / 2;
 
                 //if mid == max then target found at mid
-                if (mid == x) return mid;
+                if (arr[mid] == x) return mid;
                 
                 //if number at mid is greater than the target, then return left side of array
-                if (arr[mid] > x)
-                {
-                    return binarySearch(arr, min, mid - 1, x);
-                }
+                if (arr[mid] > x) return binarySearch(arr, min, mid - 1, x);
 
                 //if number at mid is less than the target, then return right side of array
-                return binarySearch(arr, mid + 1, max, x);
+                if (arr[mid] < x) return binarySearch(arr, mid + 1, max, x);
             }
 
             return -1;
--- AlgoPractice/Program.cs
+++ AlgoPractice/Program.cs
@@ -6,11 +6,10 @@ namespace AlgoPractice
     {
         static void Main(string[] args)
         {
-            int[] array = {1, 2, 3, 4, 4, 5, 6, 8};
+            int[] array = {1, 2, 3, 4, 4, 5, 6, 9, 10, 14, 67};
             int l = 0;
             int r = array.Length - 1;
-            int x = 6;
-
+            int x = 10;
             Console.WriteLine(new BinarySearch().binarySearch(array, l, r, x));
         }
     }
